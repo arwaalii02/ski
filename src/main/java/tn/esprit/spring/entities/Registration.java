848a3274d0a291313a28542978b6db20be679cf6
arwa255 <80281987+arwa255@users.noreply.github.com>
2024-11-03 19:29:05 +0100

    Update Registration.java

--- src/main/java/tn/esprit/spring/entities/Registration.java
+++ src/main/java/tn/esprit/spring/entities/Registration.java
@@ -1,12 +1,9 @@
 package tn.esprit.spring.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
@@ -36,4 +33,8 @@ public class Registration implements Serializable {
 	@JsonIgnore
 	@ManyToOne
 	Course course;
+	// addded attribute to track registartions over time
+	@Temporal(TemporalType.DATE)
+	Date registrationDate;
+
 }
