e2a0983facce8c4cfe1ab7f0e98548ccf9a372f4
GharbiiAhmed <93822936+GharbiiAhmed@users.noreply.github.com>
2024-11-07 17:46:36 +0100

    Update DatabaseIntegrationTest.java

--- src/test/java/tn/esprit/spring/DatabaseIntegrationTest.java
+++ src/test/java/tn/esprit/spring/DatabaseIntegrationTest.java
@@ -3,10 +3,17 @@ package tn.esprit.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:mysql://db:3306/stationSki?createDatabaseIfNotExist=true",
+    "spring.datasource.username=root",
+    "spring.datasource.password=2001",
+    "spring.jpa.hibernate.ddl-auto=none"  // Ensure this is consistent with your Docker Compose
+})
 public class DatabaseIntegrationTest {
 
     @Autowired
@@ -14,6 +21,7 @@ public class DatabaseIntegrationTest {
 
     @Test
     public void testMySQLConnection() {
+        // Run a simple query to test the database connection
         Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
         assertThat(result).isEqualTo(1);
     }
